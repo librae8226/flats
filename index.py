4b522462c191bce4ae3e8bca6c1e9c8f2d0b368f
Librae <librae8226@gmail.com>
2016-12-07 00:46:59 +0800

    cleanups

--- index.py
+++ index.py
@@ -5,29 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	    return 'Index Page'
+    return 'Index Page'
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-	return render_template('hello.html', name=name)
+@app.route('/api/fn')
+@app.route('/api/fn/')
+@app.route('/api/fn/<fn>', methods=['GET', 'POST'])
+def fn(fn=None):
+    """ api sample
+    <description>
+    args:
+    returns:
+    """
+    app.logger.debug('fn name: %s, method is %s' % (fn, request.method))
+    return 'fn name: %s, method is %s' % (fn, request.method)
 
-@app.route('/about')
-def about():
-	return 'The about page'
-
-@app.route('/func/<func>')
-def run_func(func):
-	return 'func name: %s' % func
-
-@app.route('/select', methods=['GET', 'POST'])
-def select():
-	print 'select entry'
-	if request.method == 'GET':
-                print 'calling select.sh'
-	else:
-		print 'do nothing'
-	return 'select process done'
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
