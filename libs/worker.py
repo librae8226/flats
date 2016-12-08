5a9ee7c2fc56c7e12a86bd2c9bc143a2818e6e24
Librae <librae8226@gmail.com>
2016-12-08 15:48:16 +0800

    save csv for report

--- libs/worker.py
+++ libs/worker.py
@@ -6,10 +6,12 @@ def get_stock_basics():
     returns: a json object containing the whole martket information
     """
     return ts.get_stock_basics().to_json('basics.json', orient='index');
+    #return ts.get_stock_basics().to_csv('basics.csv', encoding='UTF-8');
 
 def get_report_data(year, quarter):
     """ invoke tushare get_report_data with json object output
     args:
     returns: a json object containing the whole martket report in specific year, quarter
     """
-    return ts.get_report_data(int(year), int(quarter)).to_json(year+'q'+quarter+'.json', orient='index');
+    #return ts.get_report_data(int(year), int(quarter)).to_json(year+'q'+quarter+'.json', orient='index');
+    return ts.get_report_data(int(year), int(quarter)).to_csv(year+'q'+quarter+'.csv', encoding='UTF-8',index=False)
