b380fc9745e281c9138f32be199ea39b4e38f4f6
Librae <librae8226@gmail.com>
2017-04-10 22:14:37 +0800

    collect data

--- libs/worker.py
+++ libs/worker.py
@@ -2,28 +2,32 @@ import os
 import tushare as ts
 import pandas as pd
 
+PREFIX = 'data'
+
 def get_stock_basics():
-    ''' invoke tushare get_stock_basics with json object output
+    ''' invoke tushare get_stock_basics() with csv output
     args:
-    returns: a json object containing the whole martket information
+    returns: csv format data containing the whole martket information
     '''
+    filename = PREFIX + '/' + 'basics.csv'
     df = ts.get_stock_basics().sort_values(by='code')
-    return df.to_json('basics.json', orient='index');
-    #return df.to_csv('basics.csv', encoding='UTF-8');
+    return df.to_csv(filename, encoding='UTF-8');
+    #return df.to_json('basics.json', orient='index');
 
 def get_report_data(year, quarter):
-    ''' invoke tushare get_report_data with json object output
+    ''' invoke tushare get_report_data() with csv output
     brief: to improve data integrality, we repeatedly do these actions in a row,
            call API -> append to file -> drop duplicates
     args: year, quarter
-    returns: a json object containing the whole martket report in specific year, quarter
+    returns: csv format data containing the whole martket report in specific year, quarter
     '''
+    filename = PREFIX + '/' + year + 'q' + quarter + '.csv'
     df = ts.get_report_data(int(year), int(quarter)).sort_values(by='code').drop_duplicates()
-    if os.path.exists(year+'q'+quarter+'.csv'):
-        df.to_csv(year+'q'+quarter+'.csv', mode='a', header=None, encoding='UTF-8', index=False)
+    if os.path.exists(filename):
+        df.to_csv(filename, mode='a', header=None, encoding='UTF-8', index=False)
     else:
-        df.to_csv(year+'q'+quarter+'.csv', encoding='UTF-8', index=False)
+        df.to_csv(filename, encoding='UTF-8', index=False)
 
-    df = pd.read_csv(year+'q'+quarter+'.csv', dtype={'code': object}).sort_values(by='code').drop_duplicates()
-    return df.to_csv(year+'q'+quarter+'.csv', encoding='UTF-8', index=False)
+    df = pd.read_csv(filename, dtype={'code': object}).sort_values(by='code').drop_duplicates()
+    return df.to_csv(filename, encoding='UTF-8', index=False)
     #return df.to_json(year+'q'+quarter+'.json', orient='index');
